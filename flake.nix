a954324683c6da1279a14ffd27102c6e3685a408
Dwayne Crooks <me@dwaynecrooks.com>
2023-11-21 05:56:54 -0400

    Add flake-utils to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,31 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=23.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "elm-integer";
 
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "elm-integer";
+          packages = with pkgs.elmPackages; [
+            elm
+            elm-doc-preview
+            elm-format
+            elm-optimize-level-2
+            elm-test
+          ];
 
-        packages = with pkgs.elmPackages; [
-          elm
-          elm-doc-preview
-          elm-format
-          elm-optimize-level-2
-          elm-test
-        ];
-
-        shellHook =
-          ''
-          export project="$PWD"
-          export PATH="$project/bin:$PATH"
-          '';
-      };
-    };
+          shellHook =
+            ''
+            export project="$PWD"
+            export PATH="$project/bin:$PATH"
+            '';
+        };
+      }
+    );
 }
